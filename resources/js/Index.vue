def47e8ccb7ea414bf499d9a7f46ebef022548b1
Petr Nikitin <nikitablvck@gmai.com>
2020-04-19 20:10:44 +0300

    Section 3 : Diving into Vue.js

---
+++ resources/js/Index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <nav class="navbar bg-white border-bottom navbar-light">
+            <router-link class="navbar-brand mr-auto" to="/">LaravelBnb</router-link>
+            <router-link class="btn nav-button" to="/second">Second</router-link>
+        </nav>
+        <div class="container mt-4 mb-4 px-4">
+            <router-view></router-view>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Index"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/bookables/BookableListItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title"> {{ itemTitle }} </h5>
+            <p class="card-text"> {{ itemContent }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "BookableListItem",
+        props: {itemTitle: String, itemContent: String, price: Number},
+    }
+</script>
+
+<style scoped>
+
+</style>
